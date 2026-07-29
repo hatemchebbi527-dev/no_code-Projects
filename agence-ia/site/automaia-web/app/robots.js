5fd590dfd6fe971527065b4a51ad6a77e3ebc937
hatemchebbi527-dev <hatemchebbi527@gmail.com>
2026-07-29 02:38:45 +0200

    Create robots.js

---
+++ agence-ia/site/automaia-web/app/robots.js
@@ -0,0 +1,7 @@
+// Next.js App Router auto-generates /robots.txt from this file.
+export default function robots() {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: "https://automa-ia.net/sitemap.xml",
+  };
+}
